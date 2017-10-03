66f075ee7d02fe145bb09630d8ecc42e264ff8be
Cameron Squire <camsqu@gmail.com>
2017-10-03 09:55:57 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -4,16 +4,16 @@ app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  
 
 @app.route("/")
 def render_main():
-    return render_template('home.html')
+    return render_template('index.html')
 
 @app.route("/pg1")
 def render_page1():
-    return render_template('page1.html')
+    return render_template('overwatchTab.html')
 
 
 @app.route("/pg2")
 def render_page2():
-    return render_template('page2.html')
+    return render_template('pubgTab.html')
 
 
 if __name__=="__main__":
