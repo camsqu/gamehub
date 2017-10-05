14d8c8d47bcea7448e59568b62af505ae444839b
camsqu <camsqu@gmail.com>
2017-10-05 09:05:39 -0700

    fine

--- webapp.py
+++ webapp.py
@@ -6,12 +6,12 @@ app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  
 def render_main():
     return render_template('index.html')
 
-@app.route("/pg1")
+@app.route("/overwatch")
 def render_page1():
     return render_template('overwatchTab.html')
 
 
-@app.route("/pg2")
+@app.route("/pubg")
 def render_page2():
     return render_template('pubgTab.html')
 
